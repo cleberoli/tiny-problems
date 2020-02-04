bc28992bd761e21bb63b5a98dd34c3ab0bdf32bc
Cleber Oliveira <cleberoliveira.d@gmail.com>
2020-02-04 20:11:53 -0300

    adding read/write functionality to intersections objects

--- tinypy/geometry/intersections.py
+++ tinypy/geometry/intersections.py
@@ -1,3 +1,4 @@
+import pickle
 from pprint import pformat
 
 from typing import List
@@ -7,13 +8,25 @@ from tinypy.lp import IntersectionProblem
 
 class Intersections:
 
-    def __init__(self, cones: dict, hyperplanes: List['Hyperplane'], dim: int, region: 'Region' = None):
+    def __init__(self, cones: dict, hyperplanes: List['Hyperplane'], dim: int, region: 'Region' = None, intersections: dict = None, write=True):
         self.__cones = cones
         self.__hyperplanes = hyperplanes
         self.__dim = dim
         self.__region = region if region is not None else Region(dim)
-        self.__intersections = dict()
-        self.compute()
+
+        if intersections is None:
+            self.__intersections = dict()
+            self.compute()
+        else:
+            self.__intersections = intersections
+
+        if write:
+            self.write('test')
+
+    @classmethod
+    def from_file(cls, filename: str):
+        intersection = cls.read(filename)
+        return cls(intersection.__cones, intersection.__hyperplanes, intersection.__dim, intersection.__region, intersection.__intersections, False)
 
     @property
     def intersections(self):
@@ -25,6 +38,15 @@ class Intersections:
 
             for h in range(len(self.__hyperplanes)):
                 self.__intersections[c][h] = self.__get_cone_intersection(c, h)
+
+    def write(self, filename: str):
+        with open(filename, 'wb') as file:
+            pickle.dump(self, file)
+
+    @staticmethod
+    def read(filename: str):
+        with open(filename, 'rb') as file:
+            return pickle.load(file)
 
     def get_intersection(self, cone: int, hyperplane: int):
         return self.__intersections[cone][hyperplane]
