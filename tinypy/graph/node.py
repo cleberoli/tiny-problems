1c9b9a275cc95cd6d478409ac0667de82a9c9995
Cleber Oliveira <cleberoliveira.d@gmail.com>
2020-02-17 20:54:08 -0300

    fix bug, change choose_hyperplane function

--- tinypy/graph/node.py
+++ tinypy/graph/node.py
@@ -26,9 +26,9 @@ class Node:
         self.right = None
 
         print(self.__path)
-        print(self.__hyperplane)
+        # print(self.__hyperplane)
         # print(self.__region)
-        print(self.__intersections)
+        # print(self.__intersections)
         # print(self.__intersections.positions)
 
     @property
@@ -56,17 +56,18 @@ class Node:
         return dict((key, value) for (key, value) in self.__cones.items() if key not in self.__intersections.positions[self.__hyperplane].left)
 
     def add_left_node(self):
-        self.left = Node(self.__height, self.left_cones, self.__hyperplanes, self.__dim, self.__left_region, f'{self.__path}_{self.__hyperplane}L')
+        self.left = Node(self.__height + 1, self.left_cones, self.__hyperplanes, self.__dim, self.__left_region, f'{self.__path}'
+                                                                                                                 f'_{self.__hyperplane}L')
         return self.left
 
     def add_right_node(self):
-        self.right = Node(self.__height, self.right_cones, self.__hyperplanes, self.__dim, self.__right_region, f'{self.__path}_{self.__hyperplane}R')
+        self.right = Node(self.__height + 1, self.right_cones, self.__hyperplanes, self.__dim, self.__right_region, f'{self.__path}'
+                                                                                                                 f'_{self.__hyperplane}R')
         return self.right
 
     def choose_hyperplane(self) -> int:
         max_cuts = [self.__f(len(self.__intersections.positions[h].left), len(self.__intersections.positions[h].right), len(self.__cones))
                     for h in self.__hyperplanes.keys()]
-        print(max_cuts)
         max_cut = max(max_cuts)
 
         for h in self.__hyperplanes.keys():
--- tinypy/graph/tree.py
+++ tinypy/graph/tree.py
@@ -13,15 +13,19 @@ class Tree:
         self.__name = name
         self.__queue = []
         self.__root = None
+        self.__height = 0
+        print(len(hyperplanes))
 
     def make_tree(self):
         self.__root = Node(0, self.__cones, self.__hyperplanes, self.__dim, Region(self.__dim), f'{self.__name}_T')
         self.__queue.append(self.__root)
         self.bfs()
+        print(self.__height)
 
     def bfs(self):
         while len(self.__queue) > 0:
-            node = self.__queue.pop()
+            node = self.__queue.pop(0)
+            self.__height = node.height if node.height > self.__height else self.__height
 
             if len(node.left_cones) > 1 and len(node.right_cones) > 1:
                 left = node.add_left_node()
