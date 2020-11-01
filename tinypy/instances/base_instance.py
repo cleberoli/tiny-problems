be692a97a75ecb0ef262c6fc8c4affaed81f5834
Cleber Oliveira <cleberoliveira.d@gmail.com>
2020-11-01 20:53:01 -0300

    [#11] updating instance

--- tinypy/instances/base_instance.py
+++ tinypy/instances/base_instance.py
@@ -1,17 +1,14 @@
 from abc import ABC, abstractmethod
-from datetime import datetime
-from os import path
 from typing import Dict, List
 
 from tinypy.geometry.point import Point
-from tinypy.utils.file import create_directory, get_full_path
+from tinypy.models.instance import Instance as DBInstance
 
 
 class Instance(ABC):
     """Base class that generates instance for different problems.
 
     Attributes:
-        instance_file: The path where the instance should be stored.
         name: Instance name.
         type: Instance type.
         dimension: Instance dimension.
@@ -26,7 +23,7 @@ class Instance(ABC):
     dimension: int
     size: int
     n: int
-    solutions: List['Point']
+    solutions: List[Point]
 
     def __init__(self, name: str, instance_type: str, dimension: int, size: int, n: int):
         """Initializes the instance.
@@ -43,21 +40,23 @@ class Instance(ABC):
         self.dimension = dimension
         self.size = size
         self.n = n
-        self.instance_file = get_full_path('files', 'instances', self.type, f'{self.name}.tpif')
-        create_directory(get_full_path('files', 'instances', self.type))
 
-        if path.exists(self.instance_file):
-            self.solutions = self.__read_instance_file()
+        instance = DBInstance(self.name, self.type, self.dimension, self.size)
+        doc = instance.get_doc()
+
+        if doc is not None:
+            self.solutions = list(instance.solutions.values())
         else:
             self.solutions = self.generate_solutions()
-            self.__write_instance_file()
+            instance.solutions = self.get_solution_dict()
+            instance.add_doc()
 
-    def get_solution_list(self) -> List['Point']:
+    def get_solution_list(self) -> List[Point]:
         """Returns the solutions as list of points.
         """
         return self.solutions
 
-    def get_solution_dict(self) -> Dict[int, 'Point']:
+    def get_solution_dict(self) -> Dict[int, Point]:
         """Returns the solutions as dictionary.
         """
         return dict((key + 1, self.solutions[key]) for key in range(len(self.solutions)))
@@ -84,7 +83,7 @@ class Instance(ABC):
         return min_solution
 
     @abstractmethod
-    def generate_solutions(self) -> List['Point']:  # pragma: no cover
+    def generate_solutions(self) -> List[Point]:  # pragma: no cover
         """Generate the solution list.
 
         Returns:
@@ -101,41 +100,3 @@ class Instance(ABC):
             by a list of three vertices.
         """
         pass
-
-    def __read_instance_file(self) -> List['Point']:
-        """Loads the instance from file.
-
-        Returns:
-            The solution list.
-        """
-        solutions = []
-
-        with open(self.instance_file, 'r') as file:
-            file.readline()             # name
-            file.readline()             # type
-            file.readline()             # generated
-            file.readline()             # dimension
-            file.readline()             # size
-            file.readline()
-
-            for i in range(self.size):
-                coords = list(map(int, file.readline().split()))
-                solutions.append(Point(coords))
-
-        return solutions
-
-    def __write_instance_file(self):
-        """Writes the instance to a file.
-        """
-        now = datetime.now()
-
-        with open(self.instance_file, 'w+') as file:
-            file.write(f'NAME: {self.name}\n')
-            file.write(f'TYPE: {self.type.upper()}\n')
-            file.write(f'GENERATED: {now.strftime("%d/%m/%Y %H:%M:%S")}\n')
-            file.write(f'DIMENSION: {self.solutions[0].dim}\n')
-            file.write(f'SIZE: {len(self.solutions)}\n\n')
-
-            for solution in self.solutions:
-                solution_str = ' '.join(map(str, solution))
-                file.write(f'{solution_str}\n')
--- tinypy/models/instance.py
+++ tinypy/models/instance.py
@@ -2,6 +2,7 @@ from typing import Dict, List
 
 from pymongo.collection import Collection
 
+from tinypy.geometry.point import Point
 from tinypy.models.db_model import DBModel
 from tinypy.utils.db import INSTANCES
 
@@ -12,18 +13,18 @@ class Instance(DBModel):
     type: str
     dimension: int
     size: int
-    points: Dict[int, List[int]]
+    solutions: Dict[int, Point]
 
-    def __init__(self, name: str, type: str, dimension: int, size: int, points: Dict[int, List[int]]):
+    def __init__(self, name: str, type: str, dimension: int, size: int, solutions: Dict[int, Point] = None):
         self.name = name
         self.type = type
         self.dimension = dimension
         self.size = size
-        self.points = points
+        self.solutions = dict() if solutions is None else solutions
 
     @classmethod
     def from_doc(cls, doc: dict) -> DBModel:
-        instance = Instance(doc['name'], doc['type'], doc['dimension'], doc['size'], doc['points'])
+        instance = Instance(doc['name'], doc['type'], doc['dimension'], doc['size'], doc['solutions'])
         instance.load_doc(doc)
 
         return instance
@@ -33,18 +34,23 @@ class Instance(DBModel):
         return INSTANCES
 
     def load_doc(self, doc: dict):
-        self.name = doc['name']
-        self.type = doc['type']
-        self.dimension = doc['dimension']
-        self.size = doc['size']
-        self.points = doc['points']
+        self.id = str(doc['_id'])
+
+        for (key, value) in doc['solutions'].items():
+            point = Point(value)
+            self.solutions[int(key)] = point
 
     def get_repr(self) -> dict:
+        solutions = dict()
+
+        for (key, value) in self.solutions.items():
+            solutions[f'{key}'] = list(value.coords)
+
         return {'name': self.name,
                 'type': self.type,
                 'dimension': self.dimension,
                 'size': self.size,
-                'points': self.points}
+                'solutions': solutions}
 
     def get_query(self) -> dict:
         return {'name': self.name}
